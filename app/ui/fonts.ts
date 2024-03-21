a5191e78ef6b9332d24b5dc2621ae339b84bacdb
Anderson0421 <jelow0413@gmail.com>
2024-03-21 16:05:02 -0500

    fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,9 @@
-import { Inter } from 'next/font/google'
+import { Inter, Lusitana } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin']})
 
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700']})
+
+// Colocamos latin como subset para que se cargue el alfabeto latino en la fuente
+// De esta forma, si el texto contiene caracteres latinos, se mostrarán correctamente
+// y aporta en el rendimiento de la carga de la fuente.
